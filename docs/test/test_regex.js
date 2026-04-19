204fe6f242a2a4546db6c880b3609d4f6b961067
Sakthi Priyan H <sakthipriyans@gmail.com>
2026-04-19 20:24:25 +0530

    Removed unwanted files

--- docs/test/test_regex.js
+++
@@ -1,5 +0,0 @@
-const txnRegex = /(\d{2}-[A-Za-z]{3}-\d{4})\s+([\d,().-]+)\s+([\d,().]+)?\s+([\d,().]+)?\s+(.*)/;
-const match1 = "01-Dec-2023 49,997.50 258.599 193.3398".match(txnRegex);
-console.log("match1:", match1);
-const match2 = "03-Feb-2025 69,996.50 2,631.368 26.6008".match(txnRegex);
-console.log("match2:", match2);
